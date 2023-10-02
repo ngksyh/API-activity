8dfff6cc8fc673d6be24262eda1248bb03564405
Yuhan Su <songyokkan@gmail.com>
2023-10-01 23:42:13 -0400

    test

--- src/APICall.java
+++ src/APICall.java
@@ -1,2 +1,39 @@
+import okhttp3.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 public class APICall {
+
+
+
+
+    public Grade getGrade(String utorid, String course) {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url(String.format("https://grade-logging-api.chenpan.ca/grade?course=%s&utorid=%s", course, utorid))
+                .addHeader("Authorization", API_TOKEN)
+                .addHeader("Content-Type", "application/json")
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            System.out.println(response);
+            JSONObject responseBody = new JSONObject(response.body().string());
+
+            if (responseBody.getInt("status_code") == 200) {
+                JSONObject grade = responseBody.getJSONObject("grade");
+                return Grade.builder()
+                        .utorid(grade.getString("utorid"))
+                        .course(grade.getString("course"))
+                        .grade(grade.getInt("grade"))
+                        .build();
+            } else {
+                throw new RuntimeException(responseBody.getString("message"));
+            }
+        } catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
