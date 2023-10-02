579919acbb1fe4f37e1d13b2e02af06d8c75fbb5
Yuhan Su <songyokkan@gmail.com>
2023-10-02 00:07:39 -0400

    s

--- src/APICall.java
+++ src/APICall.java
@@ -3,32 +3,37 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+
 
 public class APICall {
 
 
+    public static void main(String[] args) {
+        try{
+            System.out.println(APICall.getTest());
+        }catch(Exception e){
+            System.out.println("error");
+        }
+
+    }
 
 
-    public Grade getGrade(String utorid, String course) {
+
+    public static String getTest() throws IOException {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         Request request = new Request.Builder()
-                .url(String.format("https://grade-logging-api.chenpan.ca/grade?course=%s&utorid=%s", course, utorid))
-                .addHeader("Authorization", API_TOKEN)
-                .addHeader("Content-Type", "application/json")
+                .url("http://router.project-osrm.org/route/v1/driving/13.388860,52.517037;13.397634,52.529407;13.428555,52.523219")
                 .build();
         try {
             Response response = client.newCall(request).execute();
             System.out.println(response);
             JSONObject responseBody = new JSONObject(response.body().string());
 
-            if (responseBody.getInt("status_code") == 200) {
-                JSONObject grade = responseBody.getJSONObject("grade");
-                return Grade.builder()
-                        .utorid(grade.getString("utorid"))
-                        .course(grade.getString("course"))
-                        .grade(grade.getInt("grade"))
-                        .build();
+
+            if (responseBody.getString("code").equals("Ok")) {
+                return responseBody.toString();
             } else {
                 throw new RuntimeException(responseBody.getString("message"));
             }
